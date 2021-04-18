de330fb5ce1158ed10ad6734382ed3a468547cc0
QuaziK <ykozhevn@gmail.edu>
2021-04-18 14:10:54 -0400

    Update poe_watchmap.py
    Added region key for map info
    Added region preference
    Added map mod preference

--- poe_watchmap.py
+++ poe_watchmap.py
@@ -13,25 +13,45 @@ import time
 import requests
 import threading
 
-VER = '0.2.0 (sep.2020)'
+VER = '0.3.0 (may.2021)'
 STOP = 0
+
+RUN_PREF = { # 1=run, 0=dont run
+    "Haewark Hamlet":   1,
+    "Tirn's End":       1,
+    "Lex Proxima":      1,
+    "Lex Ejoris":       1,
+    "New Vastir":       1,
+    "Glennach Cairns":  1,
+    "Valdo's Rest":     1,
+    "Lira Arthain":     1
+}
+
+MOD_PREF = { # 1=care, 0=dont care
+    'reflect':      1,
+    'noregen':      1,
+    'noleech':      1,
+    'lessres':      1
+}
 
 def parse_map_info(text):
     """
     Parse item info (from clipboard, as obtained by pressing Ctrl+C hovering an item in-game).
     """
     m = re.findall(r'^Rarity: (\w+)\r?\n(.+?)\r?\n(.+?)\r?\n', text)
-    # pprint.pprint(m)
     if not m:
         return {}
     info = {'name': m[0][1], 'rarity': m[0][0], 'type': m[0][2]}
     m = re.findall(r'^Quality: +(\d+)%', text)
+    reg = re.findall('Atlas Region: (\w+\s\w+)', text)
+    if reg == []: reg = re.findall('Atlas Region: (\w+\W\w\s\w+)', text)
+    info['region'] = reg[0]  
     info['quality'] = int(m[0]) if m else 0
     info['corrupted'] = bool(re.search('^Corrupted$', text, re.M))
     info['reflect'] = text.find('reflect')
     info['noregen'] = text.find('Players cannot Regenerate Life, Mana or Energy Shield')
     info['noleech'] = text.find('Cannot Leech Life from Monsters')
-    info['lessres'] = text.find('% maximum Player Resistances')    
+    info['lessres'] = text.find('% maximum Player Resistances')
     return info
 
 def watch_clipboard(crt_area):
@@ -54,8 +74,11 @@ def watch_clipboard(crt_area):
             if text != prev:
                 info = parse_map_info(text)
                 trade_info = None
-                if info: # Interpret map info here TODO
-                    if info['reflect'] > -1:
+                if info: # Interpret map info here
+                    if RUN_PREF[info['region']] == 0:
+                        crt_area.set("[++] {" + info['name'] + "} is in " + info['region'])
+                        print("[++] {" + info['name'] + "} is in " + info['region'])                        
+                    elif info['reflect'] > -1 and MOD_PREF['reflect']:
                         crt_area.set("[!!!] {" + info['name'] + "} REFLECT")
                         print("[!!!] {" + info['name'] + "} REFLECT")
                     elif info['noregen'] > -1:
